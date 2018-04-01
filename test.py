de4c6d90db9ec2cd0a1f54f25710661b4899725f
Leo-xh <1134086740@qq.com>
2018-04-01 14:19:44 +0800

    4.1 c/s start

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("help")
+from socket import *
+
+testSocket = socket(AF_INET, SOCK_STREAM)
+testSocket.bind(('', 60000))
+testSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+testSocket.listen(1)
+print("listening")
