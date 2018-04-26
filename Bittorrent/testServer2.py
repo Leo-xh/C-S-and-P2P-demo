5b566fc0ca96e3fe1575c39707f85194dd1de44b
Leo-xh <1134086740@qq.com>
2018-04-26 23:33:17 +0800

    handshake

---
+++ Bittorrent/testServer2.py
@@ -0,0 +1,14 @@
+import struct
+import socket
+import utils
+import random
+#from twisted.internet import reactor
+from twisted.internet import reactor
+from twisted.internet.protocol import Protocol
+from twisted.internet.protocol import ClientFactory
+
+from handshake import *
+if __name__ == '__main__':
+    reactor.listenTCP(56788,HandshakeFactory(info_hash="11111111111111111112"))
+    reactor.connectTCP('127.0.0.1',46352,HandshakeFactory(info_hash="11111111111111111112"))
+    reactor.run()
